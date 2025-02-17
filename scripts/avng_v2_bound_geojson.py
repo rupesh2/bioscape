56de84f835ea8cbb679b3053277f51e3cd3e9e73
Rupesh Shrestha <rupesh2@users.noreply.github.com>
2025-02-17 12:53:25 -0500

    Add files via upload

--- scripts/avng_v2_bound_geojson.py
+++ scripts/avng_v2_bound_geojson.py
@@ -6,7 +6,9 @@ import xarray as xr
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import box
+from shapely.ops import transform
 import s3fs
+import pyproj
 
 s3 = s3fs.S3FileSystem(anon=False)
 data = []
@@ -15,6 +17,7 @@ data = []
 for fid in s3.ls('bioscape-data/AVNG_V2/'):
     # exclude other e.g., flight line folder
     if path.basename(fid).startswith('ang'):
+
         # loop through all tile files
         for f1 in s3.ls(fid):
             # only select reflectance files
@@ -25,22 +28,35 @@ for fid in s3.ls('bioscape-data/AVNG_V2/'):
                 # open reflectance file
                 ds = xr.open_dataset(s3.open(r, mode='rb'),
                                      engine="h5netcdf")
-                # create bounding box using min,max coordinates
-                bound = box(ds.easting.min().data, ds.northing.min().data,
-                          ds.easting.max().data, ds.northing.max().data)
+                # ang projection
+                utm = xr.open_dataset(path.splitext(r)[0]+'.json',
+                                     decode_coords="all",
+                                     engine="kerchunk").rio.crs
+                # if virtual zarr does not have crs use previous
+                if utm:
+                    utm2 = utm
+                else:
+                    utm = utm2
+
+                # transformer to reproject to WGS84            
+                project = pyproj.Transformer.from_crs(utm, pyproj.CRS('EPSG:4326'),
+                                                      always_xy=True).transform
+                # create bounding box using min,max coordinates in wgs84
+                bound = transform(project, box(ds.easting.min().data,
+                                                ds.northing.min().data,
+                                                ds.easting.max().data,
+                                                ds.northing.max().data))
                 # append s3 path and bounds
-                data.append([path.basename(fid),
-                             path.splitext(r)[0]+'.nc',
-                             ds.attrs['time_coverage_end'],
-                             bound])
+                data.append([path.basename(fid), r,
+                             ds.attrs['time_coverage_end'], bound])
+
             else:
                 # print if RFL file does not exist
                 print(s3.ls(f1))
 # pandas dataframe
 df =pd.DataFrame(data, columns=['fid','RFL s3','end_time','geometry'])
 # create geopandas dataframe, crs is UTM
-gdf = gpd.GeoDataFrame(df, geometry=df.geometry, crs='32734')
-# # reproject to WGS84 and export as geojson
-gdf.to_crs('4326')[['fid','RFL s3','end_time','geometry']].to_file('ANGv2_Coverage.geojson',
-                                                        driver="GeoJSON")
-
+gdf = gpd.GeoDataFrame(df, geometry=df.geometry, crs='4326')
+# export as geojson
+gdf[['fid','RFL s3','end_time','geometry']].to_file('ANGv2_Coverage.geojson',
+                                                    driver="GeoJSON")
